b164d8c8a372cb788779b88bca55ece131837535
Joe Farah <joseph.farah100@gmail.com>
2026-08-05 07:36:44 -0400

    Continue Post 24

--- src/pages/pcomponents/post24/MissionCardV1.tsx
+++ src/pages/pcomponents/post24/MissionCardV1.tsx
@@ -1,26 +1,32 @@
+
 interface MissionProps {
-    id: number,
+
     title: string,
     workTime: number,
     restTime: number,
+    prepTime: number,
     sets: number,
     clr: string,
+  
 }
 
-export default function MissionCardV1({id, title, workTime, restTime, sets,clr} : MissionProps) {
+
+export default function MissionCardV1({ title, workTime, prepTime, sets,clr} : MissionProps) {
 
         return (
 
             <div 
             style ={{borderColor: clr}}
-            className={`border-1 p-2 m-2 border-2  rounded-md shadow-md`}>
+            className={`mission-card  border-1 p-2 m-2 border-2  rounded-md shadow-md `}>
                 <div className="flex justify-between">
-                <h4> {title} </h4>
+                <h4 className="sex">{title}</h4>
                  {/* We'll format the time next */}
-                <p className=""> {workTime * sets} seconds</p>
+                <span className="total-formatted-seconds"> {(workTime * sets)} seconds</span>
+                
                 </div>
+                <div style = {{borderColor: `${clr}`}}className="w-[100%] border-1 border-r-0 border-b-0 border-l-0  border-dashed h-2 border-top-none"></div>
                 {/* We'll format the time next */}
-                 <p className="text-zinc-400"> <i>{sets} sets of {workTime} seconds </i> </p>
+                 <span className="mission-sets"> <i>{sets} sets of {workTime} seconds </i> </span>
 
             </div>
         )
--- src/pages/pcomponents/post24/MissionsDashboard.tsx
+++ src/pages/pcomponents/post24/MissionsDashboard.tsx
@@ -1,14 +1,22 @@
 
+import "./missions.css"
 import MissionCardV1 from "./MissionCardV1"
 import MissionCardV2 from "./MissionCardV2"
-export default function MissionsDashboard() {
+import MissionCardV3 from "./MissionCardV3"
+import MissionCardV4 from "./MissionCardV4"
+
+interface DashboardProps {
+  version: number
+}
+export default function MissionsDashboard( {version}: DashboardProps) {
 
  const missions = [
-  {
+    {
     id: 1,
     title: "Quick Coding",
     workTime: 600,
     restTime: 60,
+    prepTime: 120,
     sets: 2,
     clr: "#52c22d",
   },
@@ -17,7 +25,9 @@ export default function MissionsDashboard() {
     title: "Stretching",
     workTime: 30,
     restTime: 15,
+    prepTime: 60,
     sets: 3,
+   
     clr: "#2a5ece",
   },
   {
@@ -25,36 +35,86 @@ export default function MissionsDashboard() {
     title: "Music Session",
     workTime: 1200,
     restTime: 300,
+    prepTime: 0,
     sets: 3,
     clr: "#c41c13",
+  },
+
+   {
+    id: 4,
+    title: "Short Test Session",
+    workTime: 5,
+    restTime: 10,
+    prepTime: 10,
+    sets: 1,
+    clr: "#6a0cd0",
   },
 ];
 
 
     return (
 
-      
-
-        <div>
+      <div>
       
        <h3> Missions</h3>
         <div className="mission-container grid md:grid-cols-2 sm:grid-cols-1">
             {missions.map((mission) => {
 
-                return (
+                    switch (version) {
 
-            
+                      case 1: return (
+                        <MissionCardV1
+                
+                      title = {mission.title}
+                      workTime = {mission.workTime}
+                      prepTime = {mission.prepTime}
+                      restTime = {mission.restTime}
+                      sets = {mission.sets}
+                      clr = {mission.clr}
+                      />
+                      )
 
-                      <MissionCardV2
-                    id = {mission.id}
-                    title = {mission.title}
-                    workTime = {mission.workTime}
-                    restTime = {mission.restTime}
-                    sets = {mission.sets}
-                    clr = {mission.clr}
-                    />
-            );
-        })}
+                      case 2: return (
+
+                        <MissionCardV2
+         
+                      title = {mission.title}
+                      workTime = {mission.workTime}
+                      restTime = {mission.restTime}
+                      sets = {mission.sets}
+                      clr = {mission.clr}
+                      />
+                      )
+
+                      case 3: return (
+
+                        <MissionCardV3
+              
+                      title = {mission.title}
+                      workTime = {mission.workTime}
+                      restTime = {mission.restTime}
+                      prepTime= {mission.prepTime}
+                      sets = {mission.sets}
+                      clr = {mission.clr}
+                      />
+                      )
+
+                      case 4: return (
+
+                        <MissionCardV4
+              
+                      title = {mission.title}
+                      workTime = {mission.workTime}
+                      restTime = {mission.restTime}
+                      prepTime= {mission.prepTime}
+                      sets = {mission.sets}
+                      clr = {mission.clr}
+                      />
+                      )
+                    }
+
+                    })}
+       
 
         </div>
         </div>
